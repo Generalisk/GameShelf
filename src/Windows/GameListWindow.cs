3d2da302679e834abea6aad466313d5a1f8cc042
Generalisk <80116907+Generalisk@users.noreply.github.com>
2026-01-09 03:24:46 +0000

    Update game list & home windows to prevent duplicate names
    This to prevent issues with ImGui involving duplicate names

--- src/Windows/GameListWindow.cs
+++ src/Windows/GameListWindow.cs
@@ -12,13 +12,30 @@ internal class GameListWindow : StaticWindow
         if (ImGui.Button("Home"))
             SelectedGame = null;
 
+        var previousName = "";
+        int count = 0;
+
         foreach (var game in Games)
         {
             var icon = game.Icon != null ? game.Icon.Value : MissingIcon;
 
             var posX = ImGui.GetCursorPosX();
 
-            if (ImGui.Button(new string(' ', 3) + game.Name))
+            // This to prevent issues with ImGui involving duplicate names
+            string name = game.Name;
+
+            if (name == previousName)
+            {
+                name += string.Format(" ({0})", count + 1);
+                count++;
+            }
+            else
+            {
+                previousName = game.Name;
+                count = 0;
+            }
+
+            if (ImGui.Button(new string(' ', 3) + name))
                 SelectedGame = game;
 
             ImGui.SameLine();
--- src/Windows/HomeWindow.cs
+++ src/Windows/HomeWindow.cs
@@ -14,13 +14,30 @@ internal class HomeWindow : StaticWindow
         int columns = width / 200;
         ImGui.BeginTable("Table", columns);
 
+        var previousName = "";
+        int count = 1;
+
         foreach (var game in games)
         {
             var icon = game.CoverArt != null ? game.CoverArt.Value : MissingCoverArt;
 
             ImGui.TableNextColumn();
 
-            if (rlImGui.ImageButton(game.Name, icon))
+            // This to prevent issues with ImGui involving duplicate names
+            string name = game.Name;
+
+            if (name == previousName)
+            {
+                name += string.Format(" ({0})", count + 1);
+                count++;
+            }
+            else
+            {
+                previousName = game.Name;
+                count = 1;
+            }
+
+            if (rlImGui.ImageButton(name, icon))
                 SelectedGame = game;
         }
 
